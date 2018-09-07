972498d2593e0a511e5f5049f749e9b4ae182d91
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-07 07:16:14 +0300

    update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,5 +1,9 @@
+console.log('Help: reverse на вход принимает строку');
+
 var reverse = function(str) {
 	var arr = str.split('');
 	arr.reverse();
 	return arr.join('');
 };
+
+module.exports = reverse;
